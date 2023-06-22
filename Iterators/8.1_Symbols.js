e4757e93931f9f8600a97e2fcdf52f643f534bb7
Mehul Vadgama <127029826+VadgamaMehul@users.noreply.github.com>
2023-06-22 12:04:41 +0530

    Update 8.1

--- Iterators/8.1_Symbols.js
+++ Iterators/8.1_Symbols.js
@@ -18,7 +18,16 @@
  * -global symbols only accessible in that perticular module/realm it not store in global realm
  * -thus symbol has it indevidual identity we cant do cross realm to
  * 
+ * ----------For making Symbol global
+ * -let globalsym = Symbol.for('key');
+ * -Symbol.keyFor() //to get key of global Symbol
  * 
+ * -----------Characteristics of symbols
+ * -it never coflict with Object string keys.
+ * -it cannot be read using existing reflection tool
+ * -it isnt privet
+ * -it arent coercible into primitives.
+ * -
  * 
  */
 let sym = Symbol('abc');
